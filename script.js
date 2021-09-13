d811ce96844d164cd1c8c105166e6a52d45489e7
rohithreddychaganti <rohithreddychaganti@gmail.com>
2021-09-13 12:19:51 +0530

    add index.html

--- script.js
+++ script.js
@@ -1,51 +1,85 @@
-'use strict';
- var form=document.getElementById('form');
- var username=document.getElementById('username');
- var email=document.getElementById('email');
- var password=document.getElementById('password');
- var confirmPassword=document.getElementById('confirmpassword');
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const confirmPassword = document.getElementById('confirmPassword');
 
- //function
- const showError=function(input , message ){
-   const formControl= input.parentElement;
-   formControl.className='form-control error';
-   const small=formControl.querySelector('small');
-   small.innerText= message;
- };
- const showSuccess=function(input , message ){
-  const formControl= input.parentElement;
-  formControl.className='form-control success';
-}
-const isValidEmail= function(email){
-  const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
+// Show Error function
+function showError(input, message) {
+  const formControl = input.parentElement;
+  formControl.className = 'form-control error';
+  const small = formControl.querySelector('small');
+  small.innerText = message;
 }
 
+// Show success Function
+function showSuccess(input) {
+  const formControl = input.parentElement;
+  formControl.className = 'form-control success';
+}
 
- //event listeners
- form.addEventListener('submit', (e) =>{
-   e.preventDefault();
-   if(username.value ===''){
-     showError(username,'username required')
-   }else{
-     showSuccess(username)
-   }
-   if(email.value ===''){
-    showError(email,'email required')
-  }else if(!isValidEmail(email.value)){
-    showError(email,'incorrect mail')
+// Email checking
+function checkEmail(input) {
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if (re.test(input.value.trim())) {
+    showSuccess(input);
+  } else {
+    showError(input, 'Enter a Valid Email ID');
   }
-  else{
-    showSuccess(email)
+}
+// Length of the Input
+function checkLength(input, min, max) {
+  if (input.value.length < min) {
+    showError(
+      input,
+      `${getFieldName(input)} must be at least ${min} characters`
+    );
+  } else if (input.value.length > max) {
+    showError(
+      input,
+      `${getFieldName(input)} must be less than ${max} characters`
+    );
+  } else {
+    showSuccess(input);
   }
-  if(password.value ===''){
-    showError(password,'password required')
+}
+
+// Check required fields
+function checkRequired(inputArr) {
+  let isRequired = false;
+  inputArr.forEach(function(input) {
+    if (input.value.trim() === '') {
+      showError(input, `Enter a ${getFieldName(input)} `);
+      isRequired = true;
+    } else {
+      showSuccess(input);
+    }
+  });
+
+  return isRequired;
+}
+
+
+// Password
+function checkPasswordsMatch(input1, input2) {
+  if (input1.value !== input2.value) {
+    showError(input2, 'Both Passwords do not match');
   }
-  if(confirmPassword.value ===''){
-    showError(confirmPassword,'confirmPassword required')
-  }else if(password !==confirmPassword){
-    showError(confirmPassword,'Enter password correct')
-  }else{
-    showSuccess(confirmPassword)
+}
+function getFieldName(input) {
+  return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+}
+
+// Add Event listeners
+form.addEventListener('submit', function(e) {
+  e.preventDefault();
+
+  if(!checkRequired([username, email, password, confirmPassword])){
+    checkLength(username, 3, 15);
+    checkLength(password, 6, 25);
+    checkEmail(email);
+    checkPasswordsMatch(password, confirmPassword);
+    
   }
- });
+
+});
